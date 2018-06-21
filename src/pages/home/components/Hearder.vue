26c114541227a7806753a82889a54204570f58b6
yuxiaoshuang1994 <1664226785@qq.com>
2018-06-21 15:46:28 +0800

    引入 iconfont 并使用，优化冗余代码

--- src/pages/home/components/Hearder.vue
+++ src/pages/home/components/Hearder.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arror-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -14,21 +22,26 @@
 </script>
 
 <style scoped lang="stylus">
+  @import '~styles/default.styl'
   .header
     display flex
     height .86rem
     line-height .86rem
     color #fff
-    background #00bcd4
+    background $bgColor
 
     .header-left
       float left
       width .64rem
+      .back-icon
+        text-align center
+        font-size .4rem
 
     .header-input
       flex 1
       margin-top .11rem
       margin-left .2rem
+      padding-left .2rem
       line-height .64rem
       height .64rem
       border-radius .1rem
@@ -39,5 +52,8 @@
       float right
       width 1.24rem
       text-align center
+      .arror-icon
+        margin-left -.04rem
+        font-size .24rem
 
 </style>
