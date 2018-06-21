fb7e6d513a72c4eca3fee0c7189d49c155608ba5
yuxiaoshuang1994 <1664226785@qq.com>
2018-06-21 16:23:51 +0800

    首页轮播图页面搭建

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,19 @@
 <template>
-  <home-hearder></home-hearder>
+  <div>
+    <home-hearder/>
+    <home-swiper/>
+  </div>
 </template>
 
 <script>
   import HomeHearder from './components/Hearder'
+  import HomeSwiper from './components/Swiper'
 
   export default {
     name: 'Home',
     components: {
-      HomeHearder
+      HomeHearder,
+      HomeSwiper
     }
   }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wapper">
+    <swiper :options="swiperOption" @someSwiperEvent="callback">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="wapper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeSwiper",
+    data () {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination', // 显示分页功能组件
+          loop: true // 支持循环轮播
+        },
+        swiperList: [
+          {
+            id: '0001',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/e5/fc2bfc8bfcccd402.jpg_750x200_1218ab5f.jpg'
+          },
+          {
+            id: '0002',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/fc/e47aa3e1c67bbc02.jpg_750x200_0f3eecf8.jpg'
+          },
+        ]
+      }
+    },
+    methods: {
+      callback () {
+
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+
+  /*更改 swiper 分页组件的选中颜色。 样式进行穿透，不受 scoped 限制*/
+  .wapper >>> .swiper-pagination-bullet-active
+    background #fff
+
+  .wapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 26.66%
+    background #eee
+    .wapper-img
+      width 100%
+
+
+</style>
