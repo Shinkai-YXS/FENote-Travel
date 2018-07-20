fe4e8787011d5c2c882994d035c03f4fb30e4e97
yuxiaoshuang1994 <1664226785@qq.com>
2018-07-20 09:19:51 +0800

    实现 Header 渐隐渐现效果

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,24 @@
 <template>
 	<div>
     <detail-banner/>
+    <detail-header/>
+    <div class="detail"></div>
   </div>
 </template>
 
 <script>
   import DetailBanner from './components/Banner'
+  import DetailHeader from './components/Header'
   export default {
     name: "Detail",
     components: {
-      DetailBanner
+      DetailBanner,
+      DetailHeader
     }
   }
 </script>
 
 <style scoped lang="stylus">
-
+  .detail
+    height 50rem
 </style>
